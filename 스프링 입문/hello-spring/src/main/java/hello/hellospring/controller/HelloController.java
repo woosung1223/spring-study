9794f6b115aca9011dc4f712a7d5950fbb0d7b63
woosung1223 <dntjd991223@naver.com>
2023-01-07 17:26:39 +0900

    View 환경설정

--- 스프링 입문/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ 스프링 입문/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -9,6 +9,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-
+        model.addAttribute("data", "hello!!");
+        return "hello";
     }
 }
