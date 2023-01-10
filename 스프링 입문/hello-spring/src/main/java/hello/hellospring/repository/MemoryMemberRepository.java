4ca8ce237399d029b7f45712020e561fee1186fa
woosung1223 <dntjd991223@naver.com>
2023-01-10 19:06:04 +0900

    자바 코드로 직접 스프링 빈 등록하기

--- 스프링 입문/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ 스프링 입문/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,7 +1,6 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
